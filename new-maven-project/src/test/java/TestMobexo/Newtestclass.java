3fe9b0d71581cfd8e479afadb7ea10654ee26310
AutomationTry <susan.help.test@gmail.com>
2022-05-18 19:31:26 +0300

    Second commit

--- new-maven-project/src/test/java/TestMobexo/Newtestclass.java
+++ new-maven-project/src/test/java/TestMobexo/Newtestclass.java
@@ -1,5 +1,23 @@
 package TestMobexo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Newtestclass {
 
+	private static WebDriver driver = null;
+
+	public static void searchTest() {
+		// Create driver
+
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Users\\s.koldaieva\\eclipse-workspace\\new-maven-project\\drivers\\chromedriver\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.get("https://portal-mobexo-de-frontend-sandbox.azurewebsites.net/reparaturstatus/suche");
+
+	}
 }
